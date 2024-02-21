82b3c752e7fbc736e7b8ba3492a39a2e8d5ff365
Mohan Talapaneni <vijaykumar21031997@gmail.com>
2024-02-21 13:36:30 -0500

    updated settings file

--- personalwebsite/personalwebsite/settings.py
+++ personalwebsite/personalwebsite/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-&bs_npj&tegd3q(dwby=0ryf4!hgsz@^gqr-%kmfagitv5tlkg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['.vercel.app']
 
 
 # Application definition
